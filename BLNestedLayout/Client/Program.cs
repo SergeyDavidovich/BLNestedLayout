2593bc07e433005606c4668245498384fe2962e8
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-01-13 18:06:57 +0100

    Add project files.

---
+++ BLNestedLayout/Client/Program.cs
@@ -0,0 +1,20 @@
+using BLNestedLayout;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BLNestedLayout
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
